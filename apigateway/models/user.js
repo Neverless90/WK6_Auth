49d710b32cf622b8d663c697e3a0899de1c735cf
Roy Vink <roy.vink@gmail.com>
2025-03-21 11:54:13 +0100

    Used save on files

--- apigateway/models/user.js
+++ apigateway/models/user.js
@@ -1,12 +1,12 @@
 const Mongoose = require('mongoose');
 
 const userSchema = new Mongoose.Schema({
-    isOwner:{type: Boolean},
-    uid:{type: String},
-    email:{type: String,unique:true, required:true},
-    hash:{type: String,unique:true, required:true},
-    salt:{type: String,unique:true, required:true}
+    isOwner: { type: Boolean },
+    uid: { type: String },
+    email: { type: String, unique: true, required: true },
+    hash: { type: String, unique: true, required: true },
+    salt: { type: String, unique: true, required: true }
 })
 
-const User = Mongoose.model('User',userSchema);
-module.exports =  User;
+const User = Mongoose.model('User', userSchema);
+module.exports = User;
